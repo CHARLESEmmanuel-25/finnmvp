5c0a3fa3ab090ec2c190711d1bf77ea809d86d2c
CHARLESEmm <ccharlesemmanuel@gmail.com>
2025-08-05 16:38:27 +0200

    search barr ok

--- app/router/router.js
+++ app/router/router.js
@@ -3,6 +3,7 @@ import signupController from "../controller/user.signup.controler.js";
 import signinController from "../controller/user.signincontroler.js";
 import companiesTopCaps from "../controller/Company.topcaps.js";
 import companySmallsCap from "../controller/Company.smallcap.js";
+import CompaniesSearchFilterController from "../controller/Company.searchbar.js";
 
 const router = Router();
 
@@ -12,6 +13,11 @@ router.get('/companies/topcaps', companiesTopCaps.topcapitalisation);
 
 // Route pour afficher la liste des entreprises dans le smallcap
 router.get('/companies/smallscaps', companySmallsCap.smallcapitalisation);
+
+// Route pour la barre de recherche
+router.post('/companies/search', CompaniesSearchFilterController.searchbar);
+
+
 
 
 
